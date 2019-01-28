49b8795f863e2776f059f959341bdbc452eea825
741755613@qq.com <741755613@qq.com@0e1a2743-8d03-4e5a-a60c-7e26af42b145>
2019-01-28 08:02:30 +0000

    日常提交优化
    git-svn-id: svn://svn7.apicloud.com/A6093043874032/clicli%E5%AE%A2%E6%88%B7%E7%AB%AF@3 0e1a2743-8d03-4e5a-a60c-7e26af42b145

--- script/common.js
+++ script/common.js
@@ -3,45 +3,46 @@
  **  可复用的函数功能抽离于此
  */
 
-//检测输入文本是否为空，空则true，非空则false
-_checkNullText(test) {
-    var input = /^[\s]*$/;
-    if (input.test(test)) {
-        return true;
+var util = (function () {
+    //检测输入文本是否为空，空则true，非空则false
+    function checkNullText(test) {
+        var input = /^[\s]*$/;
+        if (input.test(test)) {
+            return true;
+        }
+        return false;
     }
-    return false;
-}
 
+    //时间转换，天数，小时，分钟
+    function formatMsgTime(timespan) {
+        //获取GMT时间
+        var timeStamp = new Date(timespan);
+        var year = timeStamp.getFullYear();
+        var month = timeStamp.getMonth() + 1;
+        var day = timeStamp.getDate();
+        var hour = timeStamp.getHours();
+        var minute = timeStamp.getMinutes();
+        var second = timeStamp.getSeconds();
+        //获取当前GMT时间
+        var now = new Date();
+        var milliseconds = 0;
+        var timeSpanStr;
+        //计算时间差（时间戳格式）
+        milliseconds = now - timeStamp;
 
-//时间转换，天数，小时，分钟
-_formatMsgTime(timespan) {
-    //获取GMT时间
-    var timeStamp = new Date(timespan);
-    var year = timeStamp.getFullYear();
-    var month = timeStamp.getMonth() + 1;
-    var day = timeStamp.getDate();
-    var hour = timeStamp.getHours();
-    var minute = timeStamp.getMinutes();
-    var second = timeStamp.getSeconds();
-    //获取当前GMT时间
-    var now = new Date();
-    var milliseconds = 0;
-    var timeSpanStr;
-    //计算时间差（时间戳格式）
-    milliseconds = now - timeStamp;
-
-    if (milliseconds <= 1000 * 60 * 1) {
-        timeSpanStr = '刚刚';
-    } else if (1000 * 60 * 1 < milliseconds && milliseconds <= 1000 * 60 * 60) {
-        timeSpanStr = Math.round((milliseconds / (1000 * 60))) + '分钟前';
-    } else if (1000 * 60 * 60 * 1 < milliseconds && milliseconds <= 1000 * 60 * 60 * 24) {
-        timeSpanStr = Math.round(milliseconds / (1000 * 60 * 60)) + '小时前';
-    } else if (1000 * 60 * 60 * 24 < milliseconds && milliseconds <= 1000 * 60 * 60 * 24 * 15) {
-        timeSpanStr = Math.round(milliseconds / (1000 * 60 * 60 * 24)) + '天前';
-    } else if (milliseconds > 1000 * 60 * 60 * 24 * 15 && year == now.getFullYear()) {
-        timeSpanStr = month + '-' + day + ' ' + hour + ':' + minute;
-    } else {
-        timeSpanStr = year + '-' + month + '-' + day + ' ' + hour + ':' + minute;
+        if (milliseconds <= 1000 * 60 * 1) {
+            timeSpanStr = '刚刚';
+        } else if (1000 * 60 * 1 < milliseconds && milliseconds <= 1000 * 60 * 60) {
+            timeSpanStr = Math.round((milliseconds / (1000 * 60))) + '分钟前';
+        } else if (1000 * 60 * 60 * 1 < milliseconds && milliseconds <= 1000 * 60 * 60 * 24) {
+            timeSpanStr = Math.round(milliseconds / (1000 * 60 * 60)) + '小时前';
+        } else if (1000 * 60 * 60 * 24 < milliseconds && milliseconds <= 1000 * 60 * 60 * 24 * 15) {
+            timeSpanStr = Math.round(milliseconds / (1000 * 60 * 60 * 24)) + '天前';
+        } else if (milliseconds > 1000 * 60 * 60 * 24 * 15 && year == now.getFullYear()) {
+            timeSpanStr = month + '-' + day + ' ' + hour + ':' + minute;
+        } else {
+            timeSpanStr = year + '-' + month + '-' + day + ' ' + hour + ':' + minute;
+        }
+        return timeSpanStr;
     }
-    return timeSpanStr;
-}
+})()
